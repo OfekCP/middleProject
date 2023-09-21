aaa15e5e45cf1c906d72785cb9ac118ef1a5b5a0
OfekCP <ofekzvulunov@gmail.com>
2023-09-21 14:25:26 +0300

    Task

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -7,10 +7,11 @@ function NavBar() {
   return (
     <div className='navBarContainer'>
       <h1 id='header'>stuidy</h1>
+      <button id='login'>Log in</button>
       <nav className='navBar'>
         <ul>
           <li> <Link id='link' >Home</Link></li>
-          <li><Link id='link'>Assignment Managment</Link></li>
+          <li><Link id='link'>Task Managment</Link></li>
           <li> <Link id='link'>Calendar</Link></li>
           <li> <Link id='link'>Profile</Link></li>
         </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-
+import Tasks from './Task/Task.jsx'
+import Stopwatch from './Task/stopper.jsx'
+import NavBar from './NavBar.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+     <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
